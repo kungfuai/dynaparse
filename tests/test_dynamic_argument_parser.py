941f41a6616532a3552991ca64cdbd0bb1150b45
Michael Wharton <michael@kungfu.ai>
2021-02-25 16:40:17 -0600

    Patch sys for cli arg spec

--- tests/test_dynamic_argument_parser.py
+++ tests/test_dynamic_argument_parser.py
@@ -1,13 +1,24 @@
 import os
+import sys
 from dynamic_argument_parser import DynamicArgumentParser
 
 
 def test_parser_can_parse_from_json():
-    parser = DynamicArgumentParser()
-    cli_args = ["--config_values", os.path.join("sample", "values.json")]
-    args = parser.parse_args(cli_args)
-    # assert args == 0
-    print(args)
-    assert args.boolean_parameter_1 == True
-    assert args.float_parameter_1 == 1
-    assert args.list_parameter_1 == [0]
+    argv_called = sys.argv
+    try:
+        sys.argv = [
+            "test_command",
+            "--config_schema",
+            "sample/schema.json",
+            "--config_values",
+            "sample/values.json",
+        ]
+        parser = DynamicArgumentParser()
+        args = parser.parse_args()
+        assert args.boolean_parameter_1 == True
+        assert args.float_parameter_1 == 1
+        assert args.list_parameter_1 == [0]
+    except Exception as e:
+        raise Exception(e)
+    finally:
+        sys.argv = argv_called
